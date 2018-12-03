a2975eb629ca95fec1b5cafdcaa8a3a477e1a3fc
Conrado <fabiano@nti.ufal.br>
2018-12-03 18:37:19 -0300

    Initial commit

---
+++ app/src/main/java/com/requisicoes/cursoandroid/requisicoeshttpcep/MainActivity.java
@@ -0,0 +1,96 @@
+package com.requisicoes.cursoandroid.requisicoeshttpcep;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.Buffer;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button botaoRecuperar;
+    private TextView textoresultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        botaoRecuperar = findViewById(R.id.buttonRecuperar);
+        textoresultado = findViewById(R.id.textRecuperar);
+
+
+        botaoRecuperar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MyTask task = new MyTask();
+                String urlApi = "https://blockchain.info/ticker";
+                String urlCep = "https://viacep.com.br/ws/01001000/json/";
+                task.execute(urlCep);
+            }
+        });
+
+    }
+
+    class MyTask extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            String stringUrl = strings[0];
+            InputStream inputStream = null;
+            InputStreamReader inputStreamReader = null;
+            StringBuffer buffer = null;
+            try {
+                URL url = new URL(stringUrl);
+                HttpURLConnection conexao = (HttpURLConnection) url.openConnection();
+
+                // Recuperar os dados em Bytes
+                inputStream = conexao.getInputStream();
+
+                // O inputeStreamReader lê os dados em Bytes e decodifica para caracteres
+                inputStreamReader = new InputStreamReader(inputStream);
+
+                //Objeto utilizado para leitura dos caracteres do InputStreamReader
+                BufferedReader reader = new BufferedReader(inputStreamReader);
+                buffer = new StringBuffer();
+
+                String linha = "";
+                while ( (linha =  reader.readLine())!=null ){
+                    buffer.append(linha);
+                }
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            return buffer.toString();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            textoresultado.setText(s);
+        }
+    }
+
+}
